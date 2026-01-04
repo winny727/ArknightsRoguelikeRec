37b31617c7621c8695f2b7cadd52e51522e08450
winny727 <173185221@qq.com>
2026-01-04 18:24:11 +0800

    font

--- ArknightsRoguelikeRec/Global/GlobalDefine.cs
+++ ArknightsRoguelikeRec/Global/GlobalDefine.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Reflection;
 
 namespace ArknightsRoguelikeRec
@@ -19,19 +21,11 @@ namespace ArknightsRoguelikeRec
         public const int COLUMN_MIN_NODE = 1; //每列最少节点
         public const int COLUMN_MAX_NODE = 4; //每列最多节点
 
-        public static readonly Font TEXT_FONT = new Font("宋体", 12.0f, FontStyle.Bold);
+        public const string FONT_PATH = "ArknightsRoguelikeRec.Assets.Fonts.HarmonyOS_Sans_SC_Regular.ttf";
 
-        public static readonly SKTypeface SK_TEXT_FONT =
-            LoadEmbeddedFont("ArknightsRoguelikeRec.Assets.Fonts.HarmonyOS_Sans_SC_Regular.ttf");
+        //public static readonly Font TEXT_FONT = new Font("宋体", 12.0f, FontStyle.Bold);
+        public static readonly Font TEXT_FONT = FontHelper.CreateFont(FONT_PATH, 12.0f, FontStyle.Regular);
 
-        public static SKTypeface LoadEmbeddedFont(string resourceName)
-        {
-            var asm = Assembly.GetExecutingAssembly();
-            using var stream = asm.GetManifestResourceStream(resourceName);
-            if (stream == null)
-                throw new Exception($"Font resource not found: {resourceName}");
-
-            return SKTypeface.FromStream(stream);
-        }
+        public static readonly SKTypeface SK_TEXT_FONT = FontHelper.LoadEmbeddedSKTypeface(FONT_PATH);
     }
 }
---
+++ ArknightsRoguelikeRec/Helper/FontHelper.cs
@@ -0,0 +1,60 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+public static class FontHelper
+{
+    private static PrivateFontCollection mFontCollection = new PrivateFontCollection();
+
+    public static FontFamily LoadFont(string resourceName)
+    {
+        // 如果已经加载过，直接返回
+        foreach (var ff in mFontCollection.Families)
+        {
+            if (ff.Name == resourceName)
+                return ff;
+        }
+
+        var asm = Assembly.GetExecutingAssembly();
+        using Stream fontStream = asm.GetManifestResourceStream(resourceName);
+        if (fontStream == null)
+            throw new Exception($"Embedded font not found: {resourceName}");
+
+        // 复制到 byte[]，然后添加到 PrivateFontCollection
+        byte[] fontData = new byte[fontStream.Length];
+        fontStream.Read(fontData, 0, fontData.Length);
+
+        unsafe
+        {
+            fixed (byte* pFontData = fontData)
+            {
+                mFontCollection.AddMemoryFont((IntPtr)pFontData, fontData.Length);
+            }
+        }
+
+        return mFontCollection.Families[mFontCollection.Families.Length - 1]; // 返回最后添加的字体
+    }
+
+    public static Font CreateFont(string resourceName, float size, FontStyle style = FontStyle.Regular)
+    {
+        var family = LoadFont(resourceName);
+        return new Font(family, size, style, GraphicsUnit.Pixel);
+    }
+
+    public static SKTypeface LoadEmbeddedSKTypeface(string resourceName)
+    {
+        var asm = Assembly.GetExecutingAssembly();
+        using var stream = asm.GetManifestResourceStream(resourceName);
+        if (stream == null)
+            throw new Exception($"Font resource not found: {resourceName}");
+
+        return SKTypeface.FromStream(stream);
+    }
+}
